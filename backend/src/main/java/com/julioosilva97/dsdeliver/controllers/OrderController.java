61967b7381c1f6215f866e2a4055d06abdb45578
Julio Cesar <julioosilva97@gmail.com>
2021-01-06 00:21:43 -0300

    PUT /orders

--- backend/src/main/java/com/julioosilva97/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/julioosilva97/dsdeliver/controllers/OrderController.java
@@ -4,9 +4,12 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.jaxb.SpringDataJaxb.OrderDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +42,14 @@ public class OrderController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
 				.buildAndExpand(orderDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(orderDto);
+		
+	}
+	
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id){
+		
+		
+		return ResponseEntity.ok().body(service.setDelivered(id));
 		
 	}
 
