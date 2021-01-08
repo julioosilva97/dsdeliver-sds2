bc5da4e373355a9603e11c970d7f3dbd6c0e4369
Julio Cesar <julioosilva97@gmail.com>
2021-01-08 17:33:15 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Routes from './Routes';
 
 function App() {
   return (
-    <div className="App">
+    <>
      <Routes/>
-    </div>
+     <ToastContainer/>
+    </>
   );
 }
 
